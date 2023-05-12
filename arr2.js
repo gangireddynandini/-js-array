0b372d2b5b134958d55fb4e770cf728359db910e
gangireddynandini <123799756+gangireddynandini@users.noreply.github.com>
2023-05-12 13:08:40 +0530

    Add files via upload

---
+++ arr2.js
@@ -0,0 +1,33 @@
+// const com = function (bill) {
+//     return bill <= 300 & bill >= 50 ? bill * 0.15 : bill * 0.2;
+// }
+// const b = com(358);
+// // console.log(b)
+// const arr = [200, 500, 50]
+// const tip = [com(arr[0]), com(arr[1]), com(arr[2])];
+
+// console.log('the bill is:', arr)
+// console.log('tip is ', tip)
+// console.log('total bill and tip is ', arr[0] + tip[0], arr[1] + tip[1], arr[2] + tip[2])
+// console.log('-----------------------------------------')
+// const a = {
+//     firstname: 'nandu',
+//     lastname: 'gangireddy',
+//     friends: ['divya', 'joshna', 'sweta', 'bunny'],
+//     birthyear: 1999,
+//     fine: 'true',
+//     hloo: function () {
+//         return 2022 - this.birthyear
+
+//     }
+// }
+// console.log(a.hloo())
+// a.location = 'proddatur'
+// a.mail = 'nandu@gmail.com'
+// console.log(a)
+// console.log(` her first name ${a.firstname} she has ${a.friends.length} friends one of her best friend is ${a.friends[3]}`);
+// if (a.firstname === 'nandu')
+//     console.log('yes')
+// else
+//     console.log('no')
+console.log('---------------------------------')
---
+++ arrays.js
@@ -0,0 +1,22 @@
+"strict mode"
+const arr1 = ["nandu", "bunny", "chaitu", "nanda"];
+// console.log(arr1);
+// console.log(typeof arr1);
+// arr1.push('narendra');
+// console.log(arr1)
+// arr1.pop();
+// console.log(arr1);
+// const a = arr1.indexOf('bunny');
+// console.log(a)
+// const b = arr1.includes('chaitu');
+// console.log(b);
+const f = arr1.shift()
+console.log(f)
+// console.log(arr1);
+const h = arr1.shift()
+// console.log(h)
+console.log(arr1);
+const j = arr1.unshift('gowtham')
+console.log(arr1);
+const k = arr1.unshift('abhi')
+console.log(arr1);
